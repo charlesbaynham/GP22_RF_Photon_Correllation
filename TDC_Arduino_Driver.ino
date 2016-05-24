053e98665a5ad9b61c75495e205164582127d94b
Charles Baynham <charles.baynham@npl.co.uk>
2016-05-24 12:43:48 +0100

    Loop testing list ad infinitum

--- TDC_Arduino_Driver.ino
+++ TDC_Arduino_Driver.ino
@@ -6,43 +6,45 @@ typedef List<String> myList;
 
 void loopFuncConst(const myList& testList);
 
+myList testList;
+
 void setup() {
 
 	Serial.begin(250000);
 	Serial.println("Launched");
 
-	myList testList;
-
 	testList.push_back("Hello");
 	testList.push_back("again");
 	testList.push_back("world!");
 
+}
+
+void loop() {
+
 	loopFuncConst(testList);
 
-	Serial.println("*** Normal looping ***");
+	Serial.print("*** Normal looping ***, ");
 
 	for (myList::Iterator it = testList.begin(); it != testList.end(); it++) {
-		
+
 		String& str = *it;
 		const char * buf = str.c_str();
-		
+
 		Serial.print("Normal iterator: '");
 		Serial.print(buf);
 		Serial.print("' from 0x");
-		Serial.println((uint32_t)buf, HEX);
-
-		str.remove(0,1);
+		Serial.print((uint32_t)buf, HEX);
+		Serial.print(", ");
 
 	}
 
-	loopFuncConst(testList);
-}
+	Serial.println("");
 
-void loop() {}
+}
 
 void loopFuncConst(const myList& testList)  {
 
-	Serial.println("*** Const looping ***");
+	Serial.print("*** Const looping ***, ");
 
 	myList::Iterator_const it = testList.begin();
 
@@ -54,8 +56,11 @@ void loopFuncConst(const myList& testList)  {
 		Serial.print("Normal iterator: '");
 		Serial.print(buf);
 		Serial.print("' from 0x");
-		Serial.println((uint32_t)buf, HEX);
+		Serial.print((uint32_t)buf, HEX);
+		Serial.print(", ");
 
 	}
+
+	Serial.println("");
 
 }

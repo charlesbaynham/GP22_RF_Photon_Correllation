b5da60fe638eb263cfb8260e030972c291e60bbf
Fara <fara@NPL>
2017-07-20 16:48:32 +0000

    Hooks

--- latest_hash.h
+++ latest_hash.h
@@ -2,6 +2,6 @@
  * whenever a commit or checkout is completed. 
  * 
  * If you can see this comment, you must copy the files "post-checkout"
- * and "post-commit" into the .git/hooks folder
+ * and "post-commit" into the .git/hooks folder */
 
 #define LATEST_HASH "???????"

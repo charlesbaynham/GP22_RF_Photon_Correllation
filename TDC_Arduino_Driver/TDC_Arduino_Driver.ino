db675a0a676e274f915303c05808c7d3a29e5af5
Charles Baynham <charles.baynham@npl.co.uk>
2019-10-09 16:03:53 +0100

    Fix indentation

--- TDC_Arduino_Driver/TDC_Arduino_Driver.ino
+++ TDC_Arduino_Driver/TDC_Arduino_Driver.ino
@@ -84,8 +84,8 @@ void(*resetFunc) (void) = 0;
 // Prototype definitions for measure with easy defaults
 MEASUREMENT_ERROR measure(uint32_t& out, unsigned int timeout, unsigned long& completionTime_ms, unsigned long& completionTime_us);
 MEASUREMENT_ERROR measure(uint32_t& out, unsigned int timeout=500) {
-    unsigned long dummy1, dummy2;
-    return measure(out, timeout, dummy1, dummy2);
+	unsigned long dummy1, dummy2;
+	return measure(out, timeout, dummy1, dummy2);
 }
 
 /*
@@ -200,8 +200,8 @@ void loop() {
 }
 
 void serialEvent() {
-    _start_comms_us = micros();
-    _start_comms_ms = millis();
+	_start_comms_us = micros();
+	_start_comms_ms = millis();
 
 	while (Serial.available()) {
 		char c = Serial.read();
@@ -659,44 +659,44 @@ void histogramMeasure(const ParameterLookup& params) {
 //          system clock (ms with 24h rollover) at occurrence of next START pulse or "TIMEOUT"
 void timeStart(const ParameterLookup& params) {
 
-    unsigned long timeout = atol(params[1]);
+	unsigned long timeout = atol(params[1]);
 
-    if (timeout == 0) {
-        Serial.println(F("Error parsing timeout parameter"));
-        return;
-    }
+	if (timeout == 0) {
+		Serial.println(F("Error parsing timeout parameter"));
+		return;
+	}
 
-    backup_config();
+	backup_config();
 
-    // Setup the TDC for MODE 1 //
-    bitmaskWrite(GP22::REG0, GP22::REG0_MESSB2, 0);
-    // Send registers to device
-    updateTDC(GP22::registers_data);
+	// Setup the TDC for MODE 1 //
+	bitmaskWrite(GP22::REG0, GP22::REG0_MESSB2, 0);
+	// Send registers to device
+	updateTDC(GP22::registers_data);
 
-    // Start a measurement with the given timeout
-    uint32_t dummy;
-    unsigned long arrivalTime_ms, arrivalTime_us;
-    MEASUREMENT_ERROR stat = measure(dummy, timeout, arrivalTime_ms, arrivalTime_us);
+	// Start a measurement with the given timeout
+	uint32_t dummy;
+	unsigned long arrivalTime_ms, arrivalTime_us;
+	MEASUREMENT_ERROR stat = measure(dummy, timeout, arrivalTime_ms, arrivalTime_us);
 
-    restore_config();
+	restore_config();
 
-    // If we got a START timeout, report the timeout
-    if (stat == MEASUREMENT_ERROR::TIMEOUT_START) {
-        Serial.println(F("TIMEOUT"));
-        return;
-    }
+	// If we got a START timeout, report the timeout
+	if (stat == MEASUREMENT_ERROR::TIMEOUT_START) {
+		Serial.println(F("TIMEOUT"));
+		return;
+	}
 
-    // If we got another error, report that
-    if (stat != MEASUREMENT_ERROR::NO_ERROR && stat != MEASUREMENT_ERROR::TIMEOUT_STOP) {
-        Serial.print(F("Unexpected error "));
-        Serial.println((int)stat);
-        return;
-    }
-    // Report the arrive time of the START pulse
-    double arrivalTime = clippedTime(arrivalTime_ms, arrivalTime_us);
-    // Compensate for the length of the TDC's sequence. 
-    arrivalTime -= 2.5 * 1e-3;
-    Serial.println(arrivalTime, 3);
+	// If we got another error, report that
+	if (stat != MEASUREMENT_ERROR::NO_ERROR && stat != MEASUREMENT_ERROR::TIMEOUT_STOP) {
+		Serial.print(F("Unexpected error "));
+		Serial.println((int)stat);
+		return;
+	}
+	// Report the arrive time of the START pulse
+	double arrivalTime = clippedTime(arrivalTime_ms, arrivalTime_us);
+	// Compensate for the length of the TDC's sequence. 
+	arrivalTime -= 2.5 * 1e-3;
+	Serial.println(arrivalTime, 3);
 }
 
 // Output the Arduino's system time (as returned by `millis()`) of the first
@@ -712,58 +712,58 @@ void timeStart(const ParameterLookup& params) {
 //          system clock (ms with 24h rollover) at occurrence of next pulse or "TIMEOUT"
 void timeStop1(const ParameterLookup& params) {
 
-    unsigned long timeout = atol(params[1]);
+	unsigned long timeout = atol(params[1]);
 
-    if (timeout == 0) {
-        Serial.println(F("Error parsing timeout parameter"));
-        return;
-    }
-    
-    backup_config();
+	if (timeout == 0) {
+		Serial.println(F("Error parsing timeout parameter"));
+		return;
+	}
+	
+	backup_config();
 
-    // Setup the TDC for normal MODE 2
-    setMeasurementMode2(true);
-    setupForSTARTToSTOP1Mode();
-    // But enable fire output
-    fireStartMode(true);
+	// Setup the TDC for normal MODE 2
+	setMeasurementMode2(true);
+	setupForSTARTToSTOP1Mode();
+	// But enable fire output
+	fireStartMode(true);
 
 #ifdef DEBUG
-    dump_config();
+	dump_config();
 #endif
 
-    // Send registers to device
-    updateTDC(GP22::registers_data);
+	// Send registers to device
+	updateTDC(GP22::registers_data);
 
-    // Measure until we get a hit or the timeout expires
-    const unsigned long end_time = millis() + timeout;
-    unsigned long arrivalTime_ms, arrivalTime_us;
-    uint32_t out;
-    MEASUREMENT_ERROR stat = MEASUREMENT_ERROR::TIMEOUT_STOP;
-    while (millis() < end_time && (stat != MEASUREMENT_ERROR::NO_ERROR)) {
-    	// Timeout shouldn't matter here, since it's only applicable to the START pulse, but we're generating it
-    	// 100ms is more than enough for processing time
-    	stat = measure(out, 100, arrivalTime_ms, arrivalTime_us);
-    }
+	// Measure until we get a hit or the timeout expires
+	const unsigned long end_time = millis() + timeout;
+	unsigned long arrivalTime_ms, arrivalTime_us;
+	uint32_t out;
+	MEASUREMENT_ERROR stat = MEASUREMENT_ERROR::TIMEOUT_STOP;
+	while (millis() < end_time && (stat != MEASUREMENT_ERROR::NO_ERROR)) {
+		// Timeout shouldn't matter here, since it's only applicable to the START pulse, but we're generating it
+		// 100ms is more than enough for processing time
+		stat = measure(out, 100, arrivalTime_ms, arrivalTime_us);
+	}
 
-    // Restore the previous mode
-    restore_config();
+	// Restore the previous mode
+	restore_config();
 
-    // If we got a timeout, report it
-    if (stat == MEASUREMENT_ERROR::TIMEOUT_STOP) {
-        Serial.println(F("TIMEOUT"));
-        return;
-    }
+	// If we got a timeout, report it
+	if (stat == MEASUREMENT_ERROR::TIMEOUT_STOP) {
+		Serial.println(F("TIMEOUT"));
+		return;
+	}
 
-    // If we got another error, report that
-    if (stat != MEASUREMENT_ERROR::NO_ERROR) {
-        Serial.print(F("Unexpected error "));
-        Serial.println((int)stat);
-        return;
-    }
+	// If we got another error, report that
+	if (stat != MEASUREMENT_ERROR::NO_ERROR) {
+		Serial.print(F("Unexpected error "));
+		Serial.println((int)stat);
+		return;
+	}
 
-    // Report the arrive time of the pulse
-    double arrivalTime = clippedTime(arrivalTime_ms, arrivalTime_us);
-    Serial.println(arrivalTime, 3);
+	// Report the arrive time of the pulse
+	double arrivalTime = clippedTime(arrivalTime_ms, arrivalTime_us);
+	Serial.println(arrivalTime, 3);
 }
 
 // Report the Arduino's system time at the start and the end of communications
@@ -772,35 +772,35 @@ void timeStop1(const ParameterLookup& params) {
 //          system clock in ms with us resolution at start and end of function
 void reportTime(const ParameterLookup& params) {
 
-    // Read out the start time of the most recent Serial comms, before they're overwritten by an interrupt
-    unsigned long start_ms = _start_comms_ms;
-    unsigned long start_us = _start_comms_us;
+	// Read out the start time of the most recent Serial comms, before they're overwritten by an interrupt
+	unsigned long start_ms = _start_comms_ms;
+	unsigned long start_us = _start_comms_us;
 
-    // Convert to a double of millisecond since power-on
-    double time_start = clippedTime(start_ms, start_us);
+	// Convert to a double of millisecond since power-on
+	double time_start = clippedTime(start_ms, start_us);
 
-    // Reading for the end of comms
-    unsigned long end_ms = millis();
-    unsigned long end_us = micros();
-    const double time_end = clippedTime(end_ms, end_us);
+	// Reading for the end of comms
+	unsigned long end_ms = millis();
+	unsigned long end_us = micros();
+	const double time_end = clippedTime(end_ms, end_us);
 
-    Serial.print(time_start, 3);
-    Serial.print(", "); // Not stored in flash, since speed is critical here
-    Serial.println(time_end, 3);
+	Serial.print(time_start, 3);
+	Serial.print(", "); // Not stored in flash, since speed is critical here
+	Serial.println(time_end, 3);
 }
 
 // Convert a millis() + micros() reading into a time since the Arduino was powered on,
 // returned as a double with us precision, rolling over every 24 hours.
 double clippedTime(unsigned long ms, unsigned long us) {
-    // The Arduino micros time wraps around at the length of an unsigned long in microseconds:
-    // We'll round the millisecond count down to the most recent micros overflow, and use the microsecond
-    // count from there
-    const unsigned long max_length_of_micros_in_ms = ULONG_MAX/1000;
-    const unsigned long rounded_ms = ms / max_length_of_micros_in_ms;
-    // Clip the ms count at 1 day to avoid loss of float precision when we convert
-    const unsigned long clipped_ms = rounded_ms / (1000 * 60 * 60 * 24);
+	// The Arduino micros time wraps around at the length of an unsigned long in microseconds:
+	// We'll round the millisecond count down to the most recent micros overflow, and use the microsecond
+	// count from there
+	const unsigned long max_length_of_micros_in_ms = ULONG_MAX/1000;
+	const unsigned long rounded_ms = ms / max_length_of_micros_in_ms;
+	// Clip the ms count at 1 day to avoid loss of float precision when we convert
+	const unsigned long clipped_ms = rounded_ms / (1000 * 60 * 60 * 24);
 
-    return double(rounded_ms) + 0.001*double(us);
+	return double(rounded_ms) + 0.001*double(us);
 }
 
 // Calculate which bin in a histogram a number belongs to
@@ -966,8 +966,8 @@ MEASUREMENT_ERROR measure(uint32_t& out, unsigned int timeout, unsigned long& co
 	}
 
 	// Save the time
-    completionTime_us = micros();
-    completionTime_ms = millis();
+	completionTime_us = micros();
+	completionTime_ms = millis();
 
 	// Read the result
 	out = read_bytes(TDC_RESULT1, !(bool)bitmaskRead(GP22::REG0, GP22::REG0_CALIBRATE));
@@ -1208,7 +1208,7 @@ void testHistFunc(const ParameterLookup& params) {
 	}
 
 	long totalTime = 0;
-  double timeVar = 0;
+	double timeVar = 0;
 
 	for (int loop = 0; loop < numLoops; loop++) {
 		// Generate fake data
@@ -1219,9 +1219,9 @@ void testHistFunc(const ParameterLookup& params) {
 		// Start the clock!
 		long timerStart = micros();
 
-    // Run the histogram function, throwing away its return value
-    // "volatile" here tells the compiler not to do any funny business 
-    // like optimising away the function call completely
+	// Run the histogram function, throwing away its return value
+	// "volatile" here tells the compiler not to do any funny business 
+	// like optimising away the function call completely
 		for (int i = 0; i < arraySize; i++) {
 			volatile size_t throwAway = getHistIndex(numBins, 0, maxVal, vals[i]);
 		}
@@ -1231,32 +1231,32 @@ void testHistFunc(const ParameterLookup& params) {
 
 		long thisCycleTime = timerEnd - timerStart;
 
-    // Store time
+	// Store time
 		totalTime += thisCycleTime;
 
-    // Store varience
-    timeVar += thisCycleTime*thisCycleTime;
-   
+	// Store varience
+	timeVar += thisCycleTime*thisCycleTime;
+	 
 	}
 
 	const double timePerCycle = double(totalTime) / double(numLoops);
 	const double timePerCall = timePerCycle / double(arraySize);  
-  
-  timeVar = timeVar - numLoops * timePerCycle*timePerCycle;
+	
+	timeVar = timeVar - numLoops * timePerCycle*timePerCycle;
 
-  const double loopStdDev = sqrt(timeVar);
+	const double loopStdDev = sqrt(timeVar);
 
 	Serial.print(F("Time taken per call (us): "));
 	Serial.print(timePerCall);
 
-  Serial.print(F("  = "));
-  Serial.print(timePerCall / 1E6 * F_CPU);
-  Serial.print(F(" clock cycles @ "));
-  Serial.print(F_CPU);
-  Serial.println(F(" Hz"));
+	Serial.print(F("  = "));
+	Serial.print(timePerCall / 1E6 * F_CPU);
+	Serial.print(F(" clock cycles @ "));
+	Serial.print(F_CPU);
+	Serial.println(F(" Hz"));
 
-  Serial.print(F("Frac. std.dev. of loop times : "));
-  Serial.println(loopStdDev / timePerCycle);
+	Serial.print(F("Frac. std.dev. of loop times : "));
+	Serial.println(loopStdDev / timePerCycle);
 
 	free(vals);
 }
@@ -1349,10 +1349,10 @@ bool registerCommands(CommandHandler<numCommands>& h) {
 	h.registerCommand(COMMANDHANDLER_HASH("*RST"), 0, &reset);
 	h.registerCommand(COMMANDHANDLER_HASH("MEAS"), 1, &timedMeasure);
 	h.registerCommand(COMMANDHANDLER_HASH("SING"), 0, &singleMeasure);
-    h.registerCommand(COMMANDHANDLER_HASH("HIST"), 4, &histogramMeasure);
-    h.registerCommand(COMMANDHANDLER_HASH("STAR"), 1, &timeStart);
-    h.registerCommand(COMMANDHANDLER_HASH("STOP1"), 1, &timeStop1);
-    h.registerCommand(COMMANDHANDLER_HASH("TIME"), 0, &reportTime);
+	h.registerCommand(COMMANDHANDLER_HASH("HIST"), 4, &histogramMeasure);
+	h.registerCommand(COMMANDHANDLER_HASH("STAR"), 1, &timeStart);
+	h.registerCommand(COMMANDHANDLER_HASH("STOP1"), 1, &timeStop1);
+	h.registerCommand(COMMANDHANDLER_HASH("TIME"), 0, &reportTime);
 	h.registerCommand(COMMANDHANDLER_HASH("STAT"), 0, &getStatus);
 	h.registerCommand(COMMANDHANDLER_HASH("STAT?"), 0, &getStatus);
 	h.registerCommand(COMMANDHANDLER_HASH("SETU"), -1, &setupRegisters);
